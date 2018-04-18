461f620ebd3b43436381df0676f92adcc36d4343
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-18 16:54:25 -0500

    import of monsters array from monsters.js to server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
+const monsters = require('./monsters.js');
 
 app.use(express.static('server/public'));
 
 app.get('/', (req,res)=> {
     res.send('get / is working')
+});
+
+app.get('/monsters', (req, res) => {
+    res.send(monsters);
 });
 
 app.listen(PORT,() => {
