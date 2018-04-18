0d8d8063d732dc484b0b9f63a4209329fd541a2a
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-18 16:52:32 -0500

    static page loads client-side index.html

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+app.use(express.static('server/public'));
+
 app.get('/', (req,res)=> {
     res.send('get / is working')
 });
